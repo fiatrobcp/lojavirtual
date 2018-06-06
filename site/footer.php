58454d4d152dd63df917fafdb6d85c0f4eaf992e
fiatrob <track.almeida@gmail.com>
2018-06-05 23:50:38 -0300

    Add new functions and verification

---
+++ site/footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<div class="copy-sole">
+        <div class="conteudo-center-970">
+            <p class="c1">© 2018 |  DOM BOSCO | Todos os Direitos Reservados</p>
+            <p class="c2"><b class="desenvolvido">Desenvolvido </b>by <i>Jeferson & Robson</i></a></p>
+        </div>
+	</div>
+</footer>
